12b1abced0b6592302e02275d44b44f03c24f492
dharmendrajani <dharmendrajani05@gmail.com>
2026-08-30 01:38:43 +0530

    Time: 3 ms (55.65%), Space: 22.8 MB (59.99%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,47 +1,36 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-
+        int m=mat.size(), n=mat[0].size();
         vector<int> ans;
-
-        int row = 0, col = 0;
-        bool up = true;
-
-        while (ans.size() < m * n) {
-            ans.push_back(mat[row][col]);
-
-            if (up) {
-                if (col == n - 1) {
-                    row++;
-                    up = false;
-                }
-                else if (row == 0) {
-                    col++;
-                    up = false;
-                }
-                else {
-                    row--;
-                    col++;
-                }
+        int row=0,col=0;
+        while (row < m && col < n) {
+            while (row >= 0 && col < n) {
+                ans.push_back(mat[row][col]);
+                row--;
+                col++;
+            }
+            if (col == n) {
+                row += 2;
+                col--;
             }
             else {
-                if (row == m - 1) {
-                    col++;
-                    up = true;
-                }
-                else if (col == 0) {
-                    row++;
-                    up = true;
-                }
-                else {
-                    row++;
-                    col--;
-                }
+                row++;
+            }
+
+            while (row < m && col >= 0) {
+                ans.push_back(mat[row][col]);
+                row++;
+                col--;
+            }
+            if (row == m) {
+                row--;
+                col += 2;
+            }
+            else {
+                col++;
             }
         }
-
         return ans;
     }
 };
