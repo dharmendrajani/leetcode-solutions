074adcf173174170b28948273685cb96a465adcc
dharmendrajani <dharmendrajani05@gmail.com>
2026-08-31 00:20:53 +0530

    Time: 3 ms (21.38%), Space: 21.8 MB (70.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int currsum=0, actSum=0;
         int n=nums.size();
+        int ans=n;
         for(int i=0;i<n;i++){
-            currsum+=nums[i];
+            ans = ans^i^nums[i];
         }
-        for(int i=1;i<=n;i++){
-            actSum+=i;
-        }
-
-        int missing = actSum-currsum;
-
-        return missing;
+        return ans;
     }
 };
